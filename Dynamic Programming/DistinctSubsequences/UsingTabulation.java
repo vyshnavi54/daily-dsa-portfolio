2bd277f607d902e16908f3b94dd0a8229aa263f3
Sasanala Satya Sri Vyshnavi <sasanalavyshnavi@gmail.com>
2026-06-16 07:21:51 +0530

    Create UsingTabulation.java

---
+++ Dynamic Programming/DistinctSubsequences/UsingTabulation.java
@@ -0,0 +1,20 @@
+//Using Tabulation
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n1=s.length(),n2=t.length();
+        int dp[][]=new int[n1+1][n2+1];
+        for(int i=0;i<=n1;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(s.charAt(i-1)==t.charAt(j-1))
+                dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                else
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[n1][n2];
+        
+    }
+}
