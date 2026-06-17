7c1f11310d35afec813d9cdeae40dae9b3fa8b82
Sasanala Satya Sri Vyshnavi <sasanalavyshnavi@gmail.com>
2026-06-17 06:52:33 +0530

    Create UsingRecursion.java

---
+++ LinkedList/ReverseLL/UsingRecursion.java
@@ -0,0 +1,25 @@
+//Using Recursion
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+       return reverse(head);
+    }
+    static ListNode reverse(ListNode head){
+        if(head==null || head.next==null)
+        return head;
+        ListNode newnode=reverse(head.next);
+        ListNode front=head.next;
+        front.next=head;
+        head.next=null;
+        return newnode;
+    }
+}
