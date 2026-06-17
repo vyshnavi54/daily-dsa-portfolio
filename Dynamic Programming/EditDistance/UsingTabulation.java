75dc7e3cb6f123814ec17a877c08896049eed55d
Sasanala Satya Sri Vyshnavi <sasanalavyshnavi@gmail.com>
2026-06-17 05:36:59 +0530

    Create UsingTabulation.java

---
+++ Dynamic Programming/EditDistance/UsingTabulation.java
@@ -0,0 +1,25 @@
+//UsingTabulation
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n1=word1.length(),n2=word2.length();
+        int dp[][]=new int[n1+1][n2+1];
+        for(int j=1;j<=n2;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n1;i++){
+            dp[i][0]=i;
+        }
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1))
+                dp[i][j]=0+dp[i-1][j-1];
+                else{
+                    dp[i][j]=Math.min(1+dp[i][j-1],Math.min(1+dp[i-1][j-1],1+dp[i-1][j]));
+                }
+            }
+        }
+        return dp[n1][n2];
+        
+    }
+
+}
