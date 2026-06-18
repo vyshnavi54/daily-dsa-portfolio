101df8c8910d8dc9fb3a4c27ae469846fcf5a08c
Sasanala Satya Sri Vyshnavi <sasanalavyshnavi@gmail.com>
2026-06-18 07:39:07 +0530

    Create UsingStack.java

---
+++ LinkedList/CheckIfLLisPallindromeOrNot/UsingStack.java
@@ -0,0 +1,28 @@
+//UsingStack
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode temp=head;
+        Stack<Integer> s=new Stack<>();
+        while(temp!=null){
+            s.push(temp.val);
+            temp=temp.next;
+        }
+        temp=head;
+        while(temp!=null){
+            if(temp.val!=s.pop())
+            return false;
+            temp=temp.next;
+        }
+        return true;
+    }
+}
