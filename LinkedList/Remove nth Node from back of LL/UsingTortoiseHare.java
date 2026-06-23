f4e67076b8ad5eaaa2a9ff73fe77f7ea44fedf62
Sasanala Satya Sri Vyshnavi <sasanalavyshnavi@gmail.com>
2026-06-23 17:23:30 +0530

    Create UsingTortoiseHare.java

---
+++ LinkedList/Remove nth Node from back of LL/UsingTortoiseHare.java
@@ -0,0 +1,28 @@
+//Using TortoiseHare 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast=head,slow=head;
+        int length=0;
+        for(int i=0;i<n;i++){
+            fast=fast.next;
+        }
+        if(fast==null) return head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        slow.next=slow.next.next;
+        return head;
+        
+    }
+}
