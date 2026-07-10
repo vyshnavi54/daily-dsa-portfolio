40f2b671b8f8bc3913bbf3434eefee06af05ebba
Sasanala Satya Sri Vyshnavi <sasanalavyshnavi@gmail.com>
2026-07-10 20:02:37 +0530

    Create BinarySearch.java

---
+++ Binary Search/BinarySearch.java
@@ -0,0 +1,18 @@
+//LeetCode 704. Binary Search
+class Solution {
+    public int search(int[] nums, int target) {
+        int n=nums.length,low=0,high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(target<nums[mid]){
+                high=mid-1;
+            }
+            else if(target>nums[mid]){
+                low=mid+1;
+            }
+            else
+            return mid;
+        }
+        return -1;
+    }
+}
